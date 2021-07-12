17e33625b7ed224354b12110bc1eab4c0164cf11
강용석 <kangyongsuek@gmail.com>
2021-07-12 22:53:51 +0900

    add: 어드민 화면 구성중

---
+++ components/SideMenu/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './SideMenu.module.scss';
+
+export default function SideMenu() {
+    return (
+        <div className={styles.side_menu_wrap}>
+            sidemenu
+        </div>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import SideMenu from '../components/SideMenu';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <SideMenu/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
