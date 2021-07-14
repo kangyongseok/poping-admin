794b336680684591099b718e6da2121d25c435fe
강용석 <kangyongsuek@gmail.com>
2021-07-15 00:39:29 +0900

    modify: 상품등록 페이지 구성중

--- components/SideMenu/index.js
+++ components/SideMenu/index.js
@@ -4,20 +4,21 @@ import Link from 'next/link'
 import styles from './SideMenu.module.scss';
 
 export default function SideMenu() {
+    const router = useRouter()
     return (
         <div className={styles.side_menu_wrap}>
             <ul>
-                <li>
+                <li className={router.pathname === "/users" ? styles.on: ''}>
                     <Link href="/users">
                         회원목록
                     </Link>
                 </li>
-                <li>
+                <li className={router.pathname === "/register" ? styles.on: ''}>
                     <Link href="/register">
                         상품등록
                     </Link>
                 </li>
-                <li>
+                <li className={router.pathname === "/shoplist" ? styles.on: ''}>
                     <Link href="/shoplist">
                         상품목록
                     </Link>
--- pages/register/index.js
+++ pages/register/index.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField';
 import Contents from '@/components/Contents';
+import styles from './Register.module.scss';
 
 export default function Register() {
     return (
         <Contents>
-            상품등록
+            <h1>상품등록</h1>
+            <form className={styles.register_form}>
+                <TextField 
+                    required 
+                    id="standard-required" 
+                    label="상품명" 
+                    variant="outlined"
+                />
+                <TextField 
+                    required 
+                    id="standard-required" 
+                    label="상품가격" 
+                    variant="outlined"
+                />
+                <label htmlFor="img_upload">이미지 업로드</label>
+                <input id="img_upload" type="file"/>
+            </form>
         </Contents>
     )
 }
