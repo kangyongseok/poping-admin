ee5552ef9f84dbfd117619ed20ae5fec61719cf2
강용석 <kangyongsuek@gmail.com>
2021-07-16 23:57:37 +0900

    modify: 이미지 압축

--- pages/register/index.js
+++ pages/register/index.js
@@ -8,10 +8,18 @@ import styles from './Register.module.scss';
 export default function Register() {
     const [fileUrl, setFileUrl] = useState(null);
 
-    const handleImageUpload = (e) => {
+    const handleImageUpload = async (e) => {
         const imgFile = e.target.files[0];
         const imgUrl = URL.createObjectURL(imgFile);
         setFileUrl(imgUrl)
+        const options = { 
+            maxSizeMB: 1,          // (default: Number.POSITIVE_INFINITY)
+            maxWidthOrHeight: 500,   // compressedFile will scale down by ratio to a point that width or height is smaller than maxWidthOrHeight (default: undefined)
+            onProgress: (percent) => console.log(percent),       // optional, a function takes one progress argument (percentage from 0 to 100) 
+            useWebWorker: true
+        }
+        const compressedFile = await imageCompression(imgFile, options)
+        console.log(compressedFile)
     }
     
     return (
@@ -31,10 +39,6 @@ export default function Register() {
                     variant="outlined"
                 />
                 <div>
-                    {
-                        fileUrl && 
-                        <img src={fileUrl} />
-                    }
                     <label htmlFor="img_upload">이미지 업로드</label>
                     <input
                         id="img_upload"
@@ -44,6 +48,12 @@ export default function Register() {
                         className={styles.file_upload_input}
                         onChange={handleImageUpload}
                     />
+                    {
+                        fileUrl && 
+                        <div className={styles.thumbnail}>
+                            <img src={fileUrl} alt="img_thumbnail" />
+                        </div>
+                    }
                 </div>
             </form>
         </Contents>
