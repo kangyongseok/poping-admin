6f736cefd292a8cfcc962139563bc51ba470789c
강용석 <kangyongsuek@gmail.com>
2021-07-18 16:40:46 +0900

    add: 상품 등록 api 추가

--- pages/register/index.js
+++ pages/register/index.js
@@ -2,11 +2,16 @@ import React, { useState } from 'react'
 import Image from 'next/image'
 import imageCompression from 'browser-image-compression';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import Contents from '@/components/Contents';
 import styles from './Register.module.scss';
+import axios from 'axios';
 
 export default function Register() {
     const [fileUrl, setFileUrl] = useState(null);
+    const [stockName, setStockName] = useState(null);
+    const [stockPrice, setStockPcie] = useState(null);
+    const [imgLoading, setImgLoading] = useState(null);
 
     const handleImageUpload = async (e) => {
         const imgFile = e.target.files[0];
@@ -15,28 +20,42 @@ export default function Register() {
         const options = { 
             maxSizeMB: 1,          // (default: Number.POSITIVE_INFINITY)
             maxWidthOrHeight: 500,   // compressedFile will scale down by ratio to a point that width or height is smaller than maxWidthOrHeight (default: undefined)
-            onProgress: (percent) => console.log(percent),       // optional, a function takes one progress argument (percentage from 0 to 100) 
+            onProgress: (percent) => setImgLoading(percent),       // optional, a function takes one progress argument (percentage from 0 to 100) 
             useWebWorker: true
         }
         const compressedFile = await imageCompression(imgFile, options)
         console.log(compressedFile)
     }
+
+    const submit = async (e) => {
+        e.preventDefault();
+        const response = await axios.post('http://localhost:4000/api/stock/register', {
+            stockName,
+            stockPrice,
+        })
+        console.log(response)
+        if (response.status === 200) {
+            console.log(responseData)
+        }
+    }
     
     return (
         <Contents>
             <h1>상품등록</h1>
-            <form className={styles.register_form}>
+            <form className={styles.register_form} onSubmit={submit} >
                 <TextField
                     required
                     id="standard-required"
                     label="상품명"
                     variant="outlined"
+                    onChange={(e) => setStockName(e.target.value)}
                 />
                 <TextField
                     required
                     id="standard-required"
                     label="상품가격"
                     variant="outlined"
+                    onChange={(e) => setStockPcie(e.target.value)}
                 />
                 <div>
                     <label htmlFor="img_upload">이미지 업로드</label>
@@ -48,13 +67,13 @@ export default function Register() {
                         className={styles.file_upload_input}
                         onChange={handleImageUpload}
                     />
-                    {
-                        fileUrl && 
-                        <div className={styles.thumbnail}>
-                            <img src={fileUrl} alt="img_thumbnail" />
-                        </div>
-                    }
+                    <div className={styles.thumbnail}>
+                        {fileUrl && <img src={fileUrl} alt="img_thumbnail" />}
+                    </div>
                 </div>
+                <Button variant="contained" color="primary" onClick={submit} >
+                    상품 등록
+                </Button>
             </form>
         </Contents>
     )
